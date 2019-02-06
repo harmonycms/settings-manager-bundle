db57647c82f2d48f7781061395188bb0360daeb2
David Sanchez <david38sanchez@gmail.com>
2019-02-06 10:28:58 -0500

    Remove useless types

--- src/Model/Type.php
+++ src/Model/Type.php
@@ -8,7 +8,6 @@ use Helis\SettingsManagerBundle\Form\Type\YamlType;
 use MyCLabs\Enum\Enum;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\Extension\Core\Type\BirthdayType;
-use Symfony\Component\Form\Extension\Core\Type\ButtonType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
@@ -20,8 +19,6 @@ use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
-use Symfony\Component\Form\Extension\Core\Type\FormType;
-use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\LanguageType;
 use Symfony\Component\Form\Extension\Core\Type\LocaleType;
@@ -32,9 +29,6 @@ use Symfony\Component\Form\Extension\Core\Type\PercentType;
 use Symfony\Component\Form\Extension\Core\Type\RadioType;
 use Symfony\Component\Form\Extension\Core\Type\RangeType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
-use Symfony\Component\Form\Extension\Core\Type\ResetType;
-use Symfony\Component\Form\Extension\Core\Type\SearchType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TelType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -46,7 +40,6 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
  * Enum Type.
  * @method static Type BIRTHDAY()
  * @method static Type BOOL()
- * @method static Type BUTTON()
  * @method static Type CHECKBOX()
  * @method static Type CHOICE()
  * @method static Type COLLECTION()
@@ -54,16 +47,12 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
  * @method static Type COUNTRY()
  * @method static Type CURRENCY()
  * @method static Type DATETIME()
- * @method static Type DATETIME_IMMUTABLE()
  * @method static Type DATE()
- * @method static Type DATE_IMMUTABLE()
  * @method static Type DATE_INTERVAL()
  * @method static Type EMAIL()
  * @method static Type ENTITY()
  * @method static Type FILE()
  * @method static Type FLOAT()
- * @method static Type FORM()
- * @method static Type HIDDEN()
  * @method static Type INT()
  * @method static Type INTEGER()
  * @method static Type LANGUAGE()
@@ -75,15 +64,11 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
  * @method static Type RADIO()
  * @method static Type RANGE()
  * @method static Type REPEATED()
- * @method static Type RESET()
- * @method static Type SEARCH()
  * @method static Type STRING()
- * @method static Type SUBMIT()
  * @method static Type TEL()
  * @method static Type TEXTAREA()
  * @method static Type TEXT()
  * @method static Type TIME()
- * @method static Type TIME_IMMUTABLE()
  * @method static Type TIMEZONE()
  * @method static Type URL()
  * @method static Type YAML()
@@ -91,49 +76,40 @@ use Symfony\Component\Form\Extension\Core\Type\UrlType;
 class Type extends Enum
 {
 
-    public const BIRTHDAY           = BirthdayType::class;
-    public const BOOL               = CheckboxType::class;
-    public const BUTTON             = ButtonType::class;
-    public const CHECKBOX           = CheckboxType::class;
-    public const CHOICE             = ChoiceType::class;
-    public const COLLECTION         = CollectionType::class;
-    public const COLOR              = ColorType::class;
-    public const COUNTRY            = CountryType::class;
-    public const CURRENCY           = CurrencyType::class;
-    public const DATETIME           = DateTimeType::class;
-    public const DATETIME_IMMUTABLE = DateTimeType::class;
-    public const DATE               = DateType::class;
-    public const DATE_IMMUTABLE     = DateType::class;
-    public const DATE_INTERVAL      = DateIntervalType::class;
-    public const EMAIL              = EmailType::class;
-    public const ENTITY             = EntityType::class;
-    public const FILE               = FileType::class;
-    public const FLOAT              = NumberType::class;
-    public const FORM               = FormType::class;
-    public const HIDDEN             = HiddenType::class;
-    public const INT                = IntegerType::class;
-    public const INTEGER            = IntegerType::class;
-    public const LANGUAGE           = LanguageType::class;
-    public const LOCALE             = LocaleType::class;
-    public const MONEY              = MoneyType::class;
-    public const NUMBER             = NumberType::class;
-    public const PASSWORD           = PasswordType::class;
-    public const PERCENT            = PercentType::class;
-    public const RADIO              = RadioType::class;
-    public const RANGE              = RangeType::class;
-    public const REPEATED           = RepeatedType::class;
-    public const RESET              = ResetType::class;
-    public const SEARCH             = SearchType::class;
-    public const STRING             = TextType::class;
-    public const SUBMIT             = SubmitType::class;
-    public const TEL                = TelType::class;
-    public const TEXTAREA           = TextareaType::class;
-    public const TEXT               = TextType::class;
-    public const TIME               = TimeType::class;
-    public const TIME_IMMUTABLE     = TimeType::class;
-    public const TIMEZONE           = TimezoneType::class;
-    public const URL                = UrlType::class;
-    public const YAML               = YamlType::class;
+    public const BIRTHDAY      = BirthdayType::class;
+    public const BOOL          = CheckboxType::class;
+    public const CHECKBOX      = CheckboxType::class;
+    public const CHOICE        = ChoiceType::class;
+    public const COLLECTION    = CollectionType::class;
+    public const COLOR         = ColorType::class;
+    public const COUNTRY       = CountryType::class;
+    public const CURRENCY      = CurrencyType::class;
+    public const DATETIME      = DateTimeType::class;
+    public const DATE          = DateType::class;
+    public const DATE_INTERVAL = DateIntervalType::class;
+    public const EMAIL         = EmailType::class;
+    public const ENTITY        = EntityType::class;
+    public const FILE          = FileType::class;
+    public const FLOAT         = NumberType::class;
+    public const INT           = IntegerType::class;
+    public const INTEGER       = IntegerType::class;
+    public const LANGUAGE      = LanguageType::class;
+    public const LOCALE        = LocaleType::class;
+    public const MONEY         = MoneyType::class;
+    public const NUMBER        = NumberType::class;
+    public const PASSWORD      = PasswordType::class;
+    public const PERCENT       = PercentType::class;
+    public const RADIO         = RadioType::class;
+    public const RANGE         = RangeType::class;
+    public const REPEATED      = RepeatedType::class;
+    public const STRING        = TextType::class;
+    public const TEL           = TelType::class;
+    public const TEXTAREA      = TextareaType::class;
+    public const TEXT          = TextType::class;
+    public const TIME          = TimeType::class;
+    public const TIMEZONE      = TimezoneType::class;
+    public const URL           = UrlType::class;
+    public const YAML          = YamlType::class;
 
     /**
      * Returns all possible values as an array
