c09ef3385bd32560f07c6d6cd8a3b79c6b89eef6
David Sanchez <david.sanchez@nventive.com>
2019-03-15 15:45:09 -0400

    Register Doctrine Mapping files from directory

--- src/HarmonySettingsManagerBundle.php
+++ src/HarmonySettingsManagerBundle.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Harmony\Bundle\SettingsManagerBundle;
 
 use Acelaya\Doctrine\Type\PhpEnumType;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
 use Doctrine\DBAL\Types\Type as DoctrineType;
 use Harmony\Bundle\SettingsManagerBundle\DependencyInjection\Compiler\ProviderFactoryPass;
 use Harmony\Bundle\SettingsManagerBundle\DependencyInjection\Compiler\ProviderPass;
@@ -14,18 +15,32 @@ use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 
+/**
+ * Class HarmonySettingsManagerBundle
+ *
+ * @package Harmony\Bundle\SettingsManagerBundle
+ */
 class HarmonySettingsManagerBundle extends Bundle
 {
 
+    /**
+     * Boots the Bundle.
+     */
     public function boot()
     {
         parent::boot();
 
-        if (class_exists('Acelaya\Doctrine\Type\PhpEnumType') && !DoctrineType::hasType('setting_type_enum')) {
+        if (class_exists(PhpEnumType::class) && !DoctrineType::hasType('setting_type_enum')) {
             PhpEnumType::registerEnumType('setting_type_enum', Type::class);
         }
     }
 
+    /**
+     * Builds the bundle.
+     * It is only ever called once when the cache is empty.
+     *
+     * @param ContainerBuilder $container
+     */
     public function build(ContainerBuilder $container)
     {
         parent::build($container);
@@ -33,5 +48,12 @@ class HarmonySettingsManagerBundle extends Bundle
         $container->addCompilerPass(new ProviderFactoryPass(), PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
         $container->addCompilerPass(new ProviderPass());
         $container->addCompilerPass(new SettingsAwarePass());
+
+        $mappings = [
+            realpath(__DIR__ . '/Resources/config/doctrine-mapping') => 'Harmony\Bundle\SettingsManagerBundle\Model'
+        ];
+        if (class_exists(DoctrineOrmMappingsPass::class)) {
+            $container->addCompilerPass(DoctrineOrmMappingsPass::createXmlMappingDriver($mappings));
+        }
     }
 }
